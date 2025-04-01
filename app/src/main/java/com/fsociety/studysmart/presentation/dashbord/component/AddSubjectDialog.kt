a00369be1a57dc06978542452f999df486a2c1e7
LenouarMiloud <lenouarmiloud@gmail.com>
2025-04-01 15:04:36 -0800

    modifing the AddSubject file

--- app/src/main/java/com/fsociety/studysmart/presentation/dashbord/component/AddSubjectDialog.kt
+++ app/src/main/java/com/fsociety/studysmart/presentation/dashbord/component/AddSubjectDialog.kt
@@ -1,8 +1,29 @@
 package com.fsociety.studysmart.presentation.dashbord.component
 
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 
 @Composable
-fun AddSubjectDialog(){
-    
+fun AddSubjectDialog(
+    title: String = "Add/Update Subject",
+    onDismissRequest: ()->Unit,
+    onConfirmButtonClick: ()->Unit
+){
+    AlertDialog(
+        onDismissRequest = onDismissRequest,
+        title = { Text(text = title) },
+        text = {},
+        dismissButton = {
+            TextButton(onClick = onDismissRequest) {
+                Text(text = "Dismiss")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onConfirmButtonClick) {
+                Text(text = "Save")
+            }
+        }
+    )
 }
