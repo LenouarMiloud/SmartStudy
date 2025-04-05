e2627f794df4fb17e860be0417f8b7732caa5feb
LenouarMiloud <lenouarmiloud@gmail.com>
2025-04-04 19:16:42 -0800

    create the Subject Overview Section File

--- app/src/main/java/com/fsociety/studysmart/presentation/subject/ScreenSubject.kt
+++ app/src/main/java/com/fsociety/studysmart/presentation/subject/ScreenSubject.kt
@@ -1,12 +1,21 @@
 package com.fsociety.studysmart.presentation.subject
 
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,9 +24,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.fsociety.studysmart.presentation.dashbord.component.CardCount
 
 @Composable
 fun ScreenSubject() {
@@ -80,4 +93,44 @@ private fun ScreenSubjectTopBar(
             }
         }
     )
+}
+@Composable
+private fun SubjectOverviewSection(){
+    Row (
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        CardCount(
+            modifier = Modifier.weight(1f),
+            headingText = "Goal Study Hours",
+            count = "5"
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        CardCount(
+            modifier = Modifier.weight(1f),
+            headingText = "Study Hours",
+            count = "5"
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Box(
+            modifier = Modifier.size(75.dp),
+            contentAlignment = Alignment.Center
+        ){
+            CircularProgressIndicator(
+                modifier = Modifier.fillMaxSize(),
+                progress = 1f,
+                strokeWidth = 4.dp,
+                strokeCap = StrokeCap.Round,
+                color = MaterialTheme.colorScheme.surfaceVariant
+            )
+            CircularProgressIndicator(
+                modifier = Modifier.fillMaxSize(),
+                progress = 1f,
+                strokeWidth = 4.dp,
+                strokeCap = StrokeCap.Round,
+                color = MaterialTheme.colorScheme.surfaceVariant
+            )
+            Text(text="")
+        }
+    }
 }
