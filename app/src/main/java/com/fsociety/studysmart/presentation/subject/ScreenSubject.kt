09f8ea23e18f2ee506075a7d115d18e9c03ccbed
LenouarMiloud <lenouarmiloud@gmail.com>
2025-04-22 09:45:51 -0800

    modify the floating action button

--- app/src/main/java/com/fsociety/studysmart/presentation/subject/ScreenSubject.kt
+++ app/src/main/java/com/fsociety/studysmart/presentation/subject/ScreenSubject.kt
@@ -1,6 +1,7 @@
 package com.fsociety.studysmart.presentation.subject
 
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
@@ -26,6 +28,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -40,6 +44,10 @@ import com.fsociety.studysmart.tasks
 
 @Composable
 fun ScreenSubject() {
+
+    val listState = rememberLazyListState()
+    val isFABExpanded by remember {derivedStateOf { listState.firstVisibleItemIndex == 0 }}
+
     Scaffold (
         topBar = {
             ScreenSubjectTopBar(
@@ -51,16 +59,18 @@ fun ScreenSubject() {
         },
         floatingActionButton = {
             ExtendedFloatingActionButton(
-                onClick = {},
+                onClick = { },
                 icon = { Icon(imageVector = Icons.Default.Add,contentDescription = "Add") },
-                text = { Text(text = "Add Task") }
+                text = { Text(text = "Add Task") },
+                expanded = isFABExpanded
             )
         }
     ){paddingValue->
         LazyColumn (
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValue)
+                .padding(paddingValue),
+            state = listState
         ){
             item {
                 SubjectOverviewSection(
